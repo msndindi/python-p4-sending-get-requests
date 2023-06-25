e0c4838c9b3e9e786677b89f1ad1b0823d5a0d2b
Mutindi Hope <msndindi@outlook.com>
2023-06-25 17:49:09 +0300

    Completed Assignment

--- lib/get.py
+++ lib/get.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+
+ = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 
 response = requests.get(url)
 
